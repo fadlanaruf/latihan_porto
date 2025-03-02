50fa3a4ab5bb92352bc40696b4b210cc4b82a811
unknown <amarmaruffadlan@gmail.com>
2025-03-02 16:41:41 +0700

    ikan

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+      container: {
+        center: true, // Memusatkan container
+        padding: '16px', // Padding default
+      },
+    },
+    plugins: [],
+  };
