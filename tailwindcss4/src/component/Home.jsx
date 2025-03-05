a09c57fb4de78e36d70ea6b054d9bec224ed443e
unknown <amarmaruffadlan@gmail.com>
2025-03-05 15:24:16 +0700

    ikan

---
+++ tailwindcss4/src/component/Home.jsx
@@ -0,0 +1,39 @@
+export default function Home (){
+    return (
+        <div class="container">
+        <div class="flex items-center justify-between relative">
+          <div class="px-4">
+            <a href="index.html" class="font-bold text-lg text-blue-600 block px-8 py-6">FadlanAruf</a>
+          </div>
+          <nav class="rounded-2xl bg-gray-800 text-white flex justify-between items-center py-3 px-5">
+            <div class="space-x-4">
+              
+            </div>
+    
+                <ul class="flex items-center text-xs space-x-5 text-gray-300">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="keahlian.html">Keahlian</a>
+                    </li>
+                
+                    <li>
+                        <a href="pendidikan.html">Pendidikan</a>
+                    </li>
+                    <li>
+                        <a href="hubungi.html">Hubungi</a>
+                    </li>
+                    <li>
+                      <a href="biodata.html">Biodata</a>
+                  </li>
+                </ul>
+              
+        </nav>
+  
+         
+        </div>
+       </div>
+       
+    )
+}
--- tailwindcss4/vite.config.js
+++ tailwindcss4/vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  theme: {
-    container: {
-      center: true,
-    padding: '16px',
-      }
+  plugins: [react(), tailwindcss()],
+  base: "tailwindcss4", 
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        keahlian: path.resolve(__dirname, 'keahlian.html'),
+        biodata: path.resolve(__dirname, 'biodata.html'),
+        hubungi: path.resolve(__dirname, 'hubungi.html'),
+        pendidikan: path.resolve(__dirname, 'pendidikan.html'),
+      },
+    },
+    outDir: 'dist', 
+    emptyOutDir: true, 
   },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: "/latihan_porto",
-})
+});
